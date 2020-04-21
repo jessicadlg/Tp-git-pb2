c6467ea2dff0813b90a326795532a770d078e97a
rociovega <55218126+rociovega@users.noreply.github.com>
2020-04-21 00:13:28 -0300

    Rocio (#2)
    * modifique la función if por un swich
    
    * Agrega métodos

--- src/pb/Calculadora.java
+++ src/pb/Calculadora.java
@@ -3,29 +3,61 @@ package pb;
 import java.util.Scanner;
 
 public class Calculadora {
-	public static void main(String[] args) {
+	
+		
 		Scanner teclado = new Scanner(System.in); 
-		 Integer a;
-		 Integer b;
-		 Integer option;
-		 Integer resultado;
-		 do{
-		 System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para restar /n 3 para sumar /n 4 para dividir");
-		 option = teclado.nextInt();
-		 }while(option < 1 && option > 4);
-		 System.out.println("Ingrese el primer numero");
-		 a = teclado.nextInt();
-		 System.out.println("Ingrese el segundo numero");
-		 b = teclado.nextInt();
-		 if(option.equals(3)){
-		 resultado = a + b;
-		 }else if(option.equals(2)){
-		 resultado = a - b;
-		 }else if(option.equals(1)){
-		 resultado = a * b;
+		 Integer n1;
+		 Integer n2;
+		 Integer resultado=0;
+		 
+		 
+		 
+		 //constructor:
+		 
+		 public Calculadora (Integer num1, Integer num2) {
+			 this.n1=num1;
+			 this.n2=num2;
+		 }
+		 
+		 
+		 //metodos:
+		 
+		 public Integer sumar(Integer num1, Integer num2) {
+			 resultado = num1+num2;
+			 return resultado;
+			 
+		 }
+		 
+		 
+		 
+		 public Integer restar(Integer num1, Integer num2) {
+			 resultado = num1-num2;
+			 return resultado;
+				 }
+		 
+		 
+		 
+		 public Integer multiplicar (Integer num1, Integer num2) {
+			 resultado= num1+num2;
+			 return resultado;
+			 
+		 }
+		 
+		 
+		 public Integer dividir (Integer num1, Integer num2) {
+			 if(num2!=0) {
+				 resultado= num1/num2;
+				 return resultado;
 		 }else{
-		 resultado = a / b;
+			 System.out.println("no se puede operar");
+			 return 0;
+		 }}
+		 
+		 
+		 
+		 
+		 
+		 
+		 
 		 }
-		 System.out.println("El resultado es " + resultado.toString());
-		 }
-}
+
